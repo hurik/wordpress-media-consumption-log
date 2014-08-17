93a7fa6b7413f59477fa9073107ff6cc0882ccab
Andreas Giemza <andreas@giemza.net>
2014-08-18 01:07:48 +0200

    Quick Post and status page refactored

--- mcl-mysql-functions.php
+++ mcl-mysql-functions.php
@@ -1,11 +1,30 @@
 <?php
 
-// Source:
-// http://wordpress.org/support/topic/get-tags-specific-to-category?replies=38
-// by various people in this thread
-function get_tags_by_category($category_id) {
-    global $wpdb;
+function getAllTagsSortedByCategoryAndName($categories) {
+    // Group the data
+    $data = array();
+
+    foreach ($categories as $category) {
+        // Get the tags of the category
+        $tags = getTagsOfCategory($category->term_id);
+
+        // Group the tags by the first letter
+        foreach ($tags as $tag) {
+            // Tags which start with a number get their own group #
+            if (preg_match('/^[a-z]/i', trim($tag->name[0]))) {
+                $data[$category->term_id][$tag->name[0]][] = $tag;
+            } else {
+                $data[$category->term_id]['#'][] = $tag;
+            }
+        }
+    }
     
+    return $data;
+}
+
+function getTagsOfCategory($category_id) {
+    global $wpdb;
+
     $tags = $wpdb->get_results("
 		SELECT 
 			terms2.term_id AS tag_id,
@@ -34,23 +53,33 @@ function get_tags_by_category($category_id) {
 		GROUP BY name
 		ORDER BY name
 	");
-    
+
     // Get the link of every tag
     foreach ($tags as $tag) {
         $tag->tag_link = get_tag_link($tag->tag_id);
     }
-    
+
     // Replace the place holder with the commas
     if (!is_admin()) {
         $tags = comma_tags_filter($tags);
     }
-    
+
     return $tags;
+}
+
+function countTagsOfCategory($data, $categorie_id) {
+    $count = 0;
+
+    foreach (array_keys($data[$categorie_id]) as $key) {
+        $count += count($data[$categorie_id][$key]);
+    }
+
+    return $count;
 }
 
 function get_posts_stats($category_id) {
     global $wpdb;
-    
+
     $stats = $wpdb->get_results("
         SELECT DATE_FORMAT( post_date, '%Y-%m-%d' ) AS date, COUNT( post_date ) AS number
         FROM wp_posts p
@@ -61,13 +90,13 @@ function get_posts_stats($category_id) {
         GROUP BY DATE_FORMAT( post_date, '%Y-%m-%d' )
         ORDER BY date
 	");
-    
+
     return $stats;
 }
 
 function get_posts_stats_with_mcl_number($category_id) {
     global $wpdb;
-    
+
     $stats = $wpdb->get_results("
         SELECT DATE_FORMAT( post_date, '%Y-%m-%d' ) AS date, SUM( meta_value ) AS number
         FROM wp_posts p
@@ -80,46 +109,46 @@ function get_posts_stats_with_mcl_number($category_id) {
         GROUP BY DATE_FORMAT( post_date, '%Y-%m-%d' )
         ORDER BY date
 	");
-    
+
     return $stats;
 }
 
 function get_first_post_date() {
     global $wpdb;
-    
+
     $min_date = $wpdb->get_results("
         SELECT Min( DATE_FORMAT( post_date, '%Y-%m-%d' ) ) AS date
         FROM wp_posts
         WHERE post_status = 'publish'
         AND post_type = 'post'
 	");
-    
+
     return $min_date[0]->date;
 }
 
 function get_latest_post_of_tag_in_category_data($tag_id, $category_id) {
     // Get post with the tag
     $posts = get_posts("tag_id={$tag_id}&category={$category_id}");
-    
+
     // Get the last post
     $post = array_shift($posts);
-    
+
     return $post;
 }
 
 function get_latest_post_of_tag_in_category($tag_id, $category_id) {
     // Get the last post data
     $post = get_latest_post_of_tag_in_category_data($tag_id, $category_id);
-    
+
     // Explode the title
     $title_exploded = explode(' - ', $post->post_title);
-    
+
     // Get the last part, so we have the chapter/episode/...
     $status = array_pop($title_exploded);
-    
+
     // Get link
     $link = get_permalink($post->ID);
-    
+
     return "<a href='{$link}' title='{$post->post_title}'>{$status}</a>";
 }
 
--- mcl-status.php
+++ mcl-status.php
@@ -1,28 +1,14 @@
 <?php
 
-function mcl_status() {
+add_shortcode('mcl', 'mclStatus');
+
+function mclStatus() {
     // Get the categories
     $categories = get_categories('exclude=45,75');
-    
-    // Group the data
-    $data       = array();
-    $data_count = array();
-    foreach ($categories as $category) {
-        // Get the tags of the category
-        $tags                           = get_tags_by_category($category->term_id);
-        $data_count[$category->term_id] = count($tags);
-        
-        // Group the tags by the first letter
-        foreach ($tags as $tag) {
-            // Tags which start with a number get their own group #
-            if (preg_match('/^[a-z]/i', trim($tag->name[0]))) {
-                $data[$category->term_id][$tag->name[0]][] = $tag;
-            } else {
-                $data[$category->term_id]['#'][] = $tag;
-            }
-        }
-    }
-    
+
+    // Get the sorted data
+    $data = getAllTagsSortedByCategoryAndName($categories);
+
     // Create categories navigation
     $html = "<table border=\"1\">";
     foreach ($categories as $category) {
@@ -37,18 +23,20 @@ function mcl_status() {
                 $html .= " | ";
             }
         }
-        
+
         $html .= "</tr></td>";
     }
-    
+
     $html .= "</table>";
-    
+
     // Create the tables
     foreach ($categories as $category) {
+        $count = countTagsOfCategory($data, $category->term_id);
+
         // Category header
         $html .= "<h4 id=\"mediastatus-{$category->slug}\">{$category->name}";
-        $html .= " ({$data_count[$category->term_id]})</h4><hr />";
-        
+        $html .= " ({$count})</h4><hr />";
+
         // Create the navigation
         $html .= "<div>";
         foreach (array_keys($data[$category->term_id]) as $key) {
@@ -58,9 +46,9 @@ function mcl_status() {
                 $html .= " | ";
             }
         }
-        
+
         $html .= "</div><br />";
-        
+
         // Table
         $html .= "<table border=\"1\"><col width=\"98%\"><col width=\"1%\">";
         $html .= "<col width=\"1%\">";
@@ -73,26 +61,24 @@ function mcl_status() {
             $html .= "<th nowrap>Kapitel/Folge</th></tr>";
             foreach ($data[$category->term_id][$key] as $tag) {
                 $last_post_data = get_latest_post_of_tag_in_category($tag->tag_id, $category->term_id);
-                
+
                 if (empty($last_post_data)) {
                     continue;
                 }
-                
+
                 $name = htmlspecialchars($tag->name);
                 $name = str_replace("&amp;", "&", $name);
-                
+
                 $html .= "<tr><td><a href=\"{$tag->tag_link}\" title=\"";
                 $html .= "{$name}\">{$name}</a></td><th nowrap>{$tag->count}";
                 $html .= "</th><td nowrap>{$last_post_data}</td></tr>";
             }
         }
-        
+
         $html .= "</table>";
     }
-    
+
     return $html;
 }
-
-add_shortcode('mcl', 'mcl_status');
 
 ?>
