b597fd12e9fdbb45ec055710513926fe342f477b
Andreas Giemza <andreas@giemza.net>
2014-10-07 01:20:14 +0200

    Added monthly chart to statistics

--- mcl-admin-settings.php
+++ mcl-admin-settings.php
@@ -5,8 +5,11 @@ function mcl_settings_register() {
     register_setting( 'mcl-settings-group', 'mcl_settings_statistics_exclude_category' );
     register_setting( 'mcl-settings-group', 'mcl_settings_statistics_mcl_number' );
     register_setting( 'mcl-settings-group', 'mcl_settings_statistics_number_of_days' );
-    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_date_format' );
-    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_google_charts_options' );
+    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_daily_date_format' );
+    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_google_charts_daily_options' );
+    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_number_of_months' );
+    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_monthly_date_format' );
+    register_setting( 'mcl-settings-group', 'mcl_settings_statistics_google_charts_monthly_options' );
     register_setting( 'mcl-settings-group', 'mcl_settings_other_comma_in_tags' );
     register_setting( 'mcl-settings-group', 'mcl_settings_other_separator' );
     register_setting( 'mcl-settings-group', 'mcl_settings_other_mcl_number_and' );
@@ -46,23 +49,43 @@ function mcl_settings() {
                 </tr>
 
                 <tr>
-                    <th scope="row">Statistics size</th>
+                    <th scope="row">Daily statistics size</th>
                     <td><input type="text" name="mcl_settings_statistics_number_of_days" value="<?php echo esc_attr( get_option( 'mcl_settings_statistics_number_of_days' ) ); ?>" style="width:100%;" />
-                        <p class="description">Please inser number of days the statistic should cover. Example: 30</p>
-                </tr>
-                
-                <tr>
-                    <th scope="row">Date format</th>
-                    <td><input type="text" name="mcl_settings_statistics_date_format" value="<?php echo esc_attr( get_option( 'mcl_settings_statistics_date_format' ) ); ?>" style="width:100%;" />
-                        <p class="description">Format for dates on the statistics page. Example: j.m.Y</p>
+                        <p class="description">Please inser number of days the daily statistic should cover. Example: 30</p>
                 </tr>
 
                 <tr>
-                    <th scope="row">Google Charts Options</th>
-                    <td><textarea name="mcl_settings_statistics_google_charts_options" rows="6" style="width:100%;"><?php echo esc_attr( get_option( 'mcl_settings_statistics_google_charts_options' ) ); ?></textarea>
-                        <p class="description">When the graph gets really big it is sometime necessary to change some Google Charts options. Check the documentation for more information: <a href="https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart#StackedBars">Google Charts - Stacked Bar Charts</a><br />
+                    <th scope="row">Daily date format</th>
+                    <td><input type="text" name="mcl_settings_statistics_daily_date_format" value="<?php echo esc_attr( get_option( 'mcl_settings_statistics_daily_date_format' ) ); ?>" style="width:100%;" />
+                        <p class="description">Format for dates on the daily statistics page. Example: j.m.Y</p>
+                </tr>
+
+                <tr>
+                    <th scope="row">Daily Google Charts Options</th>
+                    <td><textarea name="mcl_settings_statistics_google_charts_daily_options" rows="6" style="width:100%;"><?php echo esc_attr( get_option( 'mcl_settings_statistics_google_charts_daily_options' ) ); ?></textarea>
+                        <p class="description">When the daily graph gets really big it is sometime necessary to change some Google Charts options. Check the documentation for more information: <a href="https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart#StackedBars">Google Charts - Stacked Bar Charts</a><br />
                             Default:<br />
                             height: data.getNumberOfRows() * 15 + 100,<br />legend: { position: 'top', maxLines: 4, alignment: 'center' },<br />bar: { groupWidth: '70%' },<br />focusTarget: 'category',<br />chartArea:{left: 100, top: 80, width: '75%', height: data.getNumberOfRows() * 15},<br />isStacked: true,</p>
+                </tr>
+
+                <tr>
+                    <th scope="row">Monthly statistics size</th>
+                    <td><input type="text" name="mcl_settings_statistics_number_of_months" value="<?php echo esc_attr( get_option( 'mcl_settings_statistics_number_of_months' ) ); ?>" style="width:100%;" />
+                        <p class="description">Please inser number of months the statistic should cover. Example: 6</p>
+                </tr>
+
+                <tr>
+                    <th scope="row">Monthly date format</th>
+                    <td><input type="text" name="mcl_settings_statistics_monthly_date_format" value="<?php echo esc_attr( get_option( 'mcl_settings_statistics_monthly_date_format' ) ); ?>" style="width:100%;" />
+                        <p class="description">Format for dates on the monthly statistics page. Example: n.Y</p>
+                </tr>
+
+                <tr>
+                    <th scope="row">Monthly Google Charts Options</th>
+                    <td><textarea name="mcl_settings_statistics_google_charts_monthly_options" rows="6" style="width:100%;"><?php echo esc_attr( get_option( 'mcl_settings_statistics_google_charts_monthly_options' ) ); ?></textarea>
+                        <p class="description">When the monthly graph gets really big it is sometime necessary to change some Google Charts options. Check the documentation for more information: <a href="https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart#StackedBars">Google Charts - Stacked Bar Charts</a><br />
+                            Default:<br />
+                            height: data.getNumberOfRows() * 15 + 100,<br />legend: { position: 'top', maxLines: 4, alignment: 'center' },<br />bar: { groupWidth: '70%' },<br />focusTarget: 'category',<br />chartArea:{left: 60, top: 80, width: '75%', height: data.getNumberOfRows() * 15},<br />isStacked: true,</p>
                 </tr>
             </table>
 
@@ -100,5 +123,4 @@ function mcl_settings() {
     </div>	
     <?php
 }
-
 ?>
--- mcl-statistics.php
+++ mcl-statistics.php
@@ -9,16 +9,16 @@ function mcl_statistics() {
 
     $start_date = date( 'Y-m-d', strtotime( "-" . get_option( 'mcl_settings_statistics_number_of_days' ) . " day", strtotime( $current_date ) ) );
 
-    $dates = array();
+    $daily_dates = array();
 
     $i = $current_date;
 
     while ( true ) {
-        array_push( $dates, $i );
+        array_push( $daily_dates, $i );
         $i = date( 'Y-m-d', strtotime( "-1 day", strtotime( $i ) ) );
 
         if ( $i == $start_date ) {
-            array_push( $dates, $i );
+            array_push( $daily_dates, $i );
             break;
         }
     }
@@ -35,7 +35,7 @@ function mcl_statistics() {
             $stats = get_post_of_category_sorted_by_date( $category->term_id );
         }
 
-        foreach ( $dates as $date ) {
+        foreach ( $daily_dates as $date ) {
             $found = 0;
 
             foreach ( $stats as $stat ) {
@@ -49,6 +49,38 @@ function mcl_statistics() {
         }
     }
 
+    $monthly_dates = array();
+
+    for ( $i = 0; $i < get_option( 'mcl_settings_statistics_number_of_months' ); $i++ ) {
+        $month = date( 'Y-m', strtotime( "-" . $i . " month", strtotime( date( 'Y-m' ) ) ) );
+        array_push( $monthly_dates, $month );
+    }
+
+    $data_month = array();
+
+    foreach ( $categories as $category ) {
+        if ( get_option( 'mcl_settings_statistics_mcl_number' ) == "1" ) {
+            $stats = get_post_with_mcl_number_of_category_sorted_by_month( $category->term_id );
+        } else {
+            $stats = get_post_of_category_sorted_by_month( $category->term_id );
+        }
+
+        
+
+        foreach ( $monthly_dates as $date ) {
+            $found = 0;
+
+            foreach ( $stats as $stat ) {
+                if ( $stat->date == $date ) {
+                    $found = $stat->number;
+                    break;
+                }
+            }
+
+            $data_month[$category->name][] = $found;
+        }
+    }
+
     $html = "
     <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>
     <script type=\"text/javascript\">
@@ -59,12 +91,13 @@ function mcl_statistics() {
     });
 
     // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
+    google.setOnLoadCallback(drawDailyChart);
+    google.setOnLoadCallback(drawMonthlyChart);
 
     // Callback that creates and populates a data table,
     // instantiates the pie chart, passes in the data and
     // draws it.
-    function drawChart() {
+    function drawDailyChart() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
             ['Date', ";
@@ -80,10 +113,10 @@ function mcl_statistics() {
     $html .= "],
 ";
 
-    for ( $i = 0; $i < count( $dates ); $i++ ) {
-        $date = DateTime::createFromFormat( 'Y-m-d', $dates[$i] );
+    for ( $i = 0; $i < count( $daily_dates ); $i++ ) {
+        $date = DateTime::createFromFormat( 'Y-m-d', $daily_dates[$i] );
 
-        $html .= "            ['{$date->format( get_option( 'mcl_settings_statistics_date_format' ) )}', ";
+        $html .= "            ['{$date->format( get_option( 'mcl_settings_statistics_daily_date_format' ) )}', ";
 
         foreach ( $categories as $category ) {
             $html .= "{$data[$category->name][$i]}";
@@ -95,7 +128,7 @@ function mcl_statistics() {
 
         $html .= "]";
 
-        if ( $i != count( $dates ) - 1 ) {
+        if ( $i != count( $daily_dates ) - 1 ) {
             $html .= ",
 ";
         }
@@ -105,27 +138,78 @@ function mcl_statistics() {
         ]);
 
         var options = {
-            " . get_option( 'mcl_settings_statistics_google_charts_options' ) . "
+            " . get_option( 'mcl_settings_statistics_google_charts_daily_options' ) . "
         };
 
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.BarChart(document.getElementById('daily_chart_div'));
         chart.draw(data, options);
+    }
+    
+    function drawMonthlyChart() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+            ['Date', ";
 
+    foreach ( $categories as $category ) {
+        $html .= "'{$category->name}'";
+
+        if ( end( $categories ) != $category ) {
+            $html .= ", ";
+        }
+    }
+
+    $html .= "],
+";
+
+    for ( $i = 0; $i < count( $monthly_dates ); $i++ ) {
+        $date = DateTime::createFromFormat( 'Y-m', $monthly_dates[$i] );
+
+        $html .= "            ['{$date->format( get_option( 'mcl_settings_statistics_monthly_date_format' ) )}', ";
+
+        foreach ( $categories as $category ) {
+            $html .= "{$data_month[$category->name][$i]}";
+
+            if ( end( $categories ) != $category ) {
+                $html .= ", ";
+            }
+        }
+
+        $html .= "]";
+
+        if ( $i != count( $daily_dates ) - 1 ) {
+            $html .= ",
+";
+        }
+    }
+
+    $html .= "
+        ]);
+
+        var options = {
+            " . get_option( 'mcl_settings_statistics_google_charts_monthly_options' ) . "
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('monthly_chart_div'));
+        chart.draw(data, options);
     }
 
     jQuery(document).ready(function($) {
         $(window).resize(function() {
-            drawChart();
+            drawDailyChart();
+            drawMonthlyChart();
         });
     });
 </script>
 
     <div>
-        <a href=\"#consumption-chart\">Konsum Diagramm</a> | <a href=\"#average-consumption\">Durchschnittlicher Konsum</a> | <a href=\"#consumption-count\">Konsum Menge</a>
+        <a href=\"#daily-consumption-chart\">Täglicher Konsum</a> | <a href=\"#monthly-consumption-chart\">Monatlicher Konsum</a> | <a href=\"#average-consumption\">Durchschnittlicher Konsum</a> | <a href=\"#consumption-count\">Konsum Menge</a>
     </div>
 
-    <h4 id=\"consumption-chart\">Konsum Diagramm</h4><hr />
-    <div id=\"chart_div\"></div>
+    <h4 id=\"daily-consumption-chart\">Täglicher Konsum</h4><hr />
+    <div id=\"daily_chart_div\"></div>
+    
+    <h4 id=\"monthly-consumption-chart\">Monatlicher Konsum</h4><hr />
+    <div id=\"monthly_chart_div\"></div>
 
     <h4 id=\"average-consumption\">Durchschnittlicher Konsum</h4><hr />
     <table border=\"1\"><col width=\"98%\"><col width=\"1%\">
@@ -159,7 +243,7 @@ function mcl_statistics() {
             <th nowrap>{$average_all}</th>
         </tr>
     </table>
-    <p>Durchschnittlich am Tag. Seit dem ersten Beitrag am {$date_first_post->format( get_option( 'mcl_settings_statistics_date_format' ) )}.</p>
+    <p>Durchschnittlich am Tag. Seit dem ersten Beitrag am {$date_first_post->format( get_option( 'mcl_settings_statistics_daily_date_format' ) )}.</p>
 
     <h4 id=\"consumption-count\">Konsum Menge</h4><hr />
     <table border=\"1\"><col width=\"98%\"><col width=\"1%\">
@@ -230,6 +314,42 @@ function get_post_of_category_sorted_by_date( $category_id ) {
         AND post_type = 'post'
         AND term_taxonomy_id = $category_id
         GROUP BY DATE_FORMAT( post_date, '%Y-%m-%d' )
+        ORDER BY date
+	" );
+
+    return $stats;
+}
+
+function get_post_with_mcl_number_of_category_sorted_by_month( $category_id ) {
+    global $wpdb;
+
+    $stats = $wpdb->get_results( "
+        SELECT DATE_FORMAT( post_date, '%Y-%m' ) AS date, SUM( meta_value ) AS number
+        FROM wp_posts p
+        LEFT OUTER JOIN wp_term_relationships r ON r.object_id = p.ID
+        LEFT OUTER JOIN wp_postmeta m ON m.post_id = p.ID
+        WHERE post_status = 'publish'
+        AND post_type = 'post'
+        AND meta_key = 'mcl_number'
+        AND term_taxonomy_id = $category_id
+        GROUP BY DATE_FORMAT( post_date, '%Y-%m' )
+        ORDER BY date
+	" );
+
+    return $stats;
+}
+
+function get_post_of_category_sorted_by_month( $category_id ) {
+    global $wpdb;
+
+    $stats = $wpdb->get_results( "
+        SELECT DATE_FORMAT( post_date, '%Y-%m' ) AS date, COUNT( post_date ) AS number
+        FROM wp_posts p
+        LEFT OUTER JOIN wp_term_relationships r ON r.object_id = p.ID
+        WHERE post_status = 'publish'
+        AND post_type = 'post'
+        AND term_taxonomy_id = $category_id
+        GROUP BY DATE_FORMAT( post_date, '%Y-%m' )
         ORDER BY date
 	" );
 
