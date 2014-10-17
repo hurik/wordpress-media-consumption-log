fdb0772658638a7609a3fca026559022c1d7d8e0
Andreas Giemza <andreas@giemza.net>
2014-10-18 00:10:35 +0200

    Quick post improved

--- mcl-admin-quick-post.php
+++ mcl-admin-quick-post.php
@@ -30,10 +30,9 @@ function mcl_quick_post() {
     foreach ( array_keys( $data ) as $cat_key ) {
         $category = get_category( $cat_key );
 
-        $cat_nav_html .= "<tr><th><div><a href=\"#mediastatus-";
+        $cat_nav_html .= "<tr><th nowrap><div><a href=\"#mediastatus-";
         $cat_nav_html .= "{$category->slug}\">{$category->name}</a>";
-        $cat_nav_html .= "</tr></th>";
-        $cat_nav_html .= "<tr><td>";
+        $cat_nav_html .= "</th><td>";
         foreach ( array_keys( $data[$category->term_id] ) as $key ) {
             $cat_nav_html .= "<a href=\"#mediastatus-{$category->slug}-";
             $cat_nav_html .= strtolower( $key ) . "\">{$key}</a>";
@@ -70,13 +69,12 @@ function mcl_quick_post() {
         $cats_html .= "</div><br />";
 
         // Table
-        $cats_html .= "<table class=\"widefat fixed\">";
-        $cats_html .= "<tr><th><strong>" . __( 'Next Post', 'media-consumption-log' ) . "</strong></th><th><strong>" . __( 'Last Post', 'media-consumption-log' ) . "</strong></th></tr>";
+        $cats_html .= "<table class=\"widefat\"><colgroup><col width=\"2%\">";
+        $cats_html .= "<col width=\"49%\"><col width=\"49%\"></colgroup>";
+        $cats_html .= "<tr><th></th><th><strong>" . __( 'Next Post', 'media-consumption-log' ) . "</strong></th><th><strong>" . __( 'Last Post', 'media-consumption-log' ) . "</strong></th></tr>";
         foreach ( array_keys( $data[$category->term_id] ) as $key ) {
-            $cats_html .= "<tr><th colspan=\"2\"><div id=\"mediastatus-";
-            $cats_html .= "{$category->slug}-" . strtolower( $key ) . "\">{$key}";
-            $cats_html .= " (" . count( $data[$category->term_id][$key] ) . ")";
-            $cats_html .= "</div></th></tr>";
+            $first = true;
+
             foreach ( $data[$category->term_id][$key] as $tag ) {
                 $last_post_data = get_last_post_of_tag_in_category_data( $tag->tag_id, $category->term_id );
 
@@ -109,13 +107,28 @@ function mcl_quick_post() {
 
                 $link = get_permalink( $last_post_data->ID );
 
-                $cats_html .= "<tr><td><a href=\"post-new.php?post_title=";
-                $cats_html .= "{$title_urlencode}&tag={$tag->tag_id}";
-                $cats_html .= "&category={$category->term_id}\" title=\"";
-                $cats_html .= "{$title}\">{$title}</a></td><td><a ";
-                $cats_html .= "href='{$link}' title='";
-                $cats_html .= "{$last_post_data->post_title}'>";
-                $cats_html .= "{$last_post_data->post_title}</a></td></tr>";
+                if ( $first ) {
+                    $cats_html .= "<tr><th nowrap><div id=\"mediastatus-";
+                    $cats_html .= "{$category->slug}-" . strtolower( $key ) . "\">{$key}";
+                    $cats_html .= " (" . count( $data[$category->term_id][$key] ) . ")";
+                    $cats_html .= "</div></th><td><a href=\"post-new.php?post_title=";
+                    $cats_html .= "{$title_urlencode}&tag={$tag->tag_id}";
+                    $cats_html .= "&category={$category->term_id}\" title=\"";
+                    $cats_html .= "{$title}\">{$title}</a></td><td><a ";
+                    $cats_html .= "href='{$link}' title='";
+                    $cats_html .= "{$last_post_data->post_title}'>";
+                    $cats_html .= "{$last_post_data->post_title}</a></td></tr>";
+
+                    $first = false;
+                } else {
+                    $cats_html .= "<tr><th nowrap></th><td><a href=\"post-new.php?post_title=";
+                    $cats_html .= "{$title_urlencode}&tag={$tag->tag_id}";
+                    $cats_html .= "&category={$category->term_id}\" title=\"";
+                    $cats_html .= "{$title}\">{$title}</a></td><td><a ";
+                    $cats_html .= "href='{$link}' title='";
+                    $cats_html .= "{$last_post_data->post_title}'>";
+                    $cats_html .= "{$last_post_data->post_title}</a></td></tr>";
+                }
             }
         }
 
@@ -126,6 +139,10 @@ function mcl_quick_post() {
         <h2>Media Consumption Log - <?php _e( 'Quick Post', 'media-consumption-log' ); ?></h2>
 
         <table class="widefat">
+            <colgroup>
+                <col width="1%">
+                <col width="99%">
+            </colgroup>
             <?php
             echo $cat_nav_html;
             ?>
