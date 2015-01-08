a2c805257349722a210580ba89201ff7804d0fd5
Andreas Giemza <andreas@giemza.net>
2015-01-08 10:11:09 +0100

    Added quick post without input

--- mcl-admin-quick-post.php
+++ mcl-admin-quick-post.php
@@ -13,9 +13,25 @@ function mcl_insert_cat_and_tag_in_new_post( $post_id ) {
     wp_set_post_categories( $post_id, array( $_REQUEST['category'] ) );
 }
 
+function mcl_create_post( $title, $tag_id, $cat_id ) {
+    $my_post = array(
+        'post_title' => urldecode( $title ),
+        'post_status' => 'publish',
+        'tags_input' => get_tag( $tag_id )->name,
+        'post_category' => array( $cat_id )
+    );
+
+    wp_insert_post( $my_post );
+}
+
 function mcl_quick_post() {
     if ( !current_user_can( 'manage_options' ) ) {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+
+    if ( isset( $_GET["title"] ) && isset( $_GET["tag_id"] ) && isset( $_GET["cat_id"] ) ) {
+        mcl_create_post( $_GET["title"], $_GET["tag_id"], $_GET["cat_id"] );
+        return;
     }
 
     // Get the categories
@@ -69,9 +85,15 @@ function mcl_quick_post() {
         $cats_html .= "</div><br />";
 
         // Table
-        $cats_html .= "<table class=\"widefat\"><colgroup><col width=\"2%\">";
-        $cats_html .= "<col width=\"49%\"><col width=\"49%\"></colgroup>";
-        $cats_html .= "<tr><th></th><th><strong>" . __( 'Next Post', 'media-consumption-log' ) . "</strong></th><th><strong>" . __( 'Last Post', 'media-consumption-log' ) . "</strong></th></tr>";
+        $cats_html .= "<table class=\"widefat\">"
+                . "<colgroup><col width=\"1%\"><col width=\"33%\"><col width=\"33%\"><col width=\"33%\"></colgroup>"
+                . "<tr>"
+                . "<th></th>"
+                . "<th><strong>" . __( 'Post Next Post', 'media-consumption-log' ) . "</strong></th>"
+                . "<th><strong>" . __( 'Modify Next Post', 'media-consumption-log' ) . "</strong></th>"
+                . "<th><strong>" . __( 'Last Post', 'media-consumption-log' ) . "</strong></th>"
+                . "</tr>";
+
         foreach ( array_keys( $data[$category->term_id] ) as $key ) {
             $first = true;
 
@@ -110,26 +132,20 @@ function mcl_quick_post() {
                 $date = DateTime::createFromFormat( "Y-m-d H:i:s", $last_post_data->post_date );
 
                 if ( $first ) {
-                    $cats_html .= "<tr><th nowrap rowspan=\"" . count( $data[$category->term_id][$key] ) . "\" valign=\"top\">";
-                    $cats_html .= "<div class= \"anchor\" id=\"mediastatus-{$category->slug}-" . strtolower( $key ) . "\"></div><div>{$key}";
-                    $cats_html .= " (" . count( $data[$category->term_id][$key] ) . ")";
-                    $cats_html .= "</div></th><td><a href=\"post-new.php?post_title=";
-                    $cats_html .= "{$title_urlencode}&tag={$tag->tag_id}";
-                    $cats_html .= "&category={$category->term_id}\" title=\"";
-                    $cats_html .= "{$title}\">{$title}</a></td><td><a ";
-                    $cats_html .= "href='{$link}' title='";
-                    $cats_html .= "{$last_post_data->post_title}'>";
-                    $cats_html .= "{$last_post_data->post_title}</a> ({$date->format( get_option( 'time_format' ) )}, {$date->format( get_option( 'mcl_settings_statistics_daily_date_format', "j.m.Y" ) )})</td></tr>";
+                    $cats_html .= "<tr>"
+                            . "<th nowrap rowspan=\"" . count( $data[$category->term_id][$key] ) . "\" valign=\"top\"><div class= \"anchor\" id=\"mediastatus-{$category->slug}-" . strtolower( $key ) . "\"></div><div>{$key} (" . count( $data[$category->term_id][$key] ) . ")</div></th>"
+                            . "<td><a class=\"quick-post\" title=\"{$title_urlencode}\" tag-id=\"{$tag->tag_id}\" cat-id=\"{$tag->cat_id}\" set-to=\"0\">{$title}</a></td>"
+                            . "<td><a href=\"post-new.php?post_title={$title_urlencode}&tag={$tag->tag_id}&category={$category->term_id}\" title=\"{$title}\">{$title}</a></td>"
+                            . "<td><a href='{$link}' title='{$last_post_data->post_title}'>{$last_post_data->post_title}</a> ({$date->format( get_option( 'time_format' ) )}, {$date->format( get_option( 'mcl_settings_statistics_daily_date_format', "j.m.Y" ) )})</td>"
+                            . "</tr>";
 
                     $first = false;
                 } else {
-                    $cats_html .= "<tr><td><a href=\"post-new.php?post_title=";
-                    $cats_html .= "{$title_urlencode}&tag={$tag->tag_id}";
-                    $cats_html .= "&category={$category->term_id}\" title=\"";
-                    $cats_html .= "{$title}\">{$title}</a></td><td><a ";
-                    $cats_html .= "href='{$link}' title='";
-                    $cats_html .= "{$last_post_data->post_title}'>";
-                    $cats_html .= "{$last_post_data->post_title}</a> ({$date->format( get_option( 'time_format' ) )}, {$date->format( get_option( 'mcl_settings_statistics_daily_date_format', "j.m.Y" ) )})</td></tr>";
+                    $cats_html .= "<tr>"
+                            . "<td><a class=\"quick-post\" title=\"{$title_urlencode}\" tag-id=\"{$tag->tag_id}\" cat-id=\"{$tag->cat_id}\" set-to=\"0\">{$title}</a></td>"
+                            . "<td><a href=\"post-new.php?post_title={$title_urlencode}&tag={$tag->tag_id}&category={$category->term_id}\" title=\"{$title}\">{$title}</a></td>"
+                            . "<td><a href='{$link}' title='{$last_post_data->post_title}'>{$last_post_data->post_title}</a> ({$date->format( get_option( 'time_format' ) )}, {$date->format( get_option( 'mcl_settings_statistics_daily_date_format', "j.m.Y" ) )})</td>"
+                            . "</tr>";
                 }
             }
         }
@@ -143,11 +159,32 @@ function mcl_quick_post() {
         @media screen and (max-width:782px) {
             div.anchor { display: block; position: relative; top: -46px; visibility: hidden; }
         }
-        
+
         @media screen and (max-width:600px) {
             div.anchor { display: block; position: relative; top: 0px; visibility: hidden; }
         }
     </style>
+
+    <script type="text/javascript">
+        var running = false;
+
+        jQuery(document).ready(function ($) {
+            $(".quick-post").click(function () {
+                if (!running) {
+                    running = true;
+
+                    $.ajax({
+                        async: false,
+                        type: 'GET',
+                        url: "admin.php?page=mcl-quick-post&title=" + $(this).attr('title') + "&tag_id=" + $(this).attr('tag-id') + "&cat_id=" + $(this).attr('cat-id'),
+                        success: function (data) {
+                            location.reload();
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 
     <div class="wrap">
         <h2>Media Consumption Log - <?php _e( 'Quick Post', 'media-consumption-log' ); ?></h2>
