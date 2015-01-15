90b308e89bdf155baaa1a95c93b8bbefb0d587ae
Andreas Giemza <andreas@giemza.net>
2015-01-15 18:18:14 +0100

    MclUnit

--- admin/MclAdminMenu.php
+++ admin/MclAdminMenu.php
@@ -1,31 +1,28 @@
 <?php
 
-MclAdminMenu::init();
+if ( is_admin() ) {
+    MclAdminMenu::init();
+}
 
 class MclAdminMenu {
 
     public static function init() {
-        add_action( 'admin_menu', array( get_called_class(), 'mcl_admin_menu' ) );
-        add_action( 'admin_bar_menu', array( get_called_class(), 'mcl_admin_bar_button' ), 75 );
+        add_action( 'admin_menu', array( get_called_class(), 'admin_menu' ) );
+        add_action( 'admin_bar_menu', array( get_called_class(), 'admin_bar_menu' ), 75 );
+        add_action( 'admin_init', array( get_called_class(), 'admin_init' ) );
     }
 
-    public static function mcl_admin_menu() {
+    public static function admin_menu() {
         // Add menu
         add_menu_page( 'MCL', 'MCL', 'manage_options', 'mcl-quick-post' );
         add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Quick Post', 'media-consumption-log' ), __( 'Quick Post', 'media-consumption-log' ), 'manage_options', 'mcl-quick-post', 'mcl_quick_post' );
         add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Complete', 'media-consumption-log' ), __( 'Complete', 'media-consumption-log' ), 'manage_options', 'mcl-complete', 'mcl_complete' );
-        add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Units', 'media-consumption-log' ), __( 'Units', 'media-consumption-log' ), 'manage_options', 'mcl-unit', 'mcl_unit' );
+        add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Units', 'media-consumption-log' ), __( 'Units', 'media-consumption-log' ), 'manage_options', 'mcl-unit', array( 'MclUnit', 'create_page' ) );
         add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Rebuild data', 'media-consumption-log' ), __( 'Rebuild data', 'media-consumption-log' ), 'manage_options', 'mcl-rebuild-data', array( 'MclRebuildData', 'create_page' ) );
         add_submenu_page( 'mcl-quick-post', 'MCL - ' . __( 'Settings', 'media-consumption-log' ), __( 'Settings', 'media-consumption-log' ), 'manage_options', 'mcl-settings', 'mcl_settings' );
-
-        // Register settings
-        add_action( 'admin_init', 'mcl_settings_register' );
-
-        // Register settings
-        add_action( 'admin_init', 'mcl_unit_register' );
     }
 
-    public static function mcl_admin_bar_button( $wp_admin_bar ) {
+    public static function admin_bar_menu( $wp_admin_bar ) {
         $args = array(
             'id' => 'mcl_admin_bar_button',
             'title' => __( 'Quick Post', 'media-consumption-log' ),
@@ -36,6 +33,11 @@ class MclAdminMenu {
         );
 
         $wp_admin_bar->add_node( $args );
+    }
+
+    public static function admin_init() {
+        mcl_settings_register();
+        MclUnit::register_settings();
     }
 
 }
---
+++ admin/MclUnit.php
@@ -0,0 +1,48 @@
+<?php
+
+class MclUnit {
+
+    public static function register_settings() {
+        $categories = get_categories( "exclude=" . MclSettingsHelper::getStatusExcludeCategory() );
+        foreach ( $categories as $category ) {
+            register_setting( 'mcl-unit-group', "mcl_unit_{$category->slug}" );
+        }
+    }
+
+    public static function create_page() {
+        if ( !current_user_can( 'manage_options' ) ) {
+            wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+        }
+        ?>
+        <div class="wrap">
+            <h2>Media Consumption Log - <?php _e( 'Units', 'media-consumption-log' ); ?></h2>
+
+            <form method="post" action="options.php">
+                <?php settings_fields( 'mcl-unit-group' ); ?>
+                <?php do_settings_sections( 'mcl-unit-group' ); ?>
+
+                <h3><?php _e( 'Units', 'media-consumption-log' ); ?></h3>
+                <p class="description"><?php _e( 'Please define the units of the categories.', 'media-consumption-log' ); ?></p>
+                <table class="form-table">
+                    <?php
+                    $categories = get_categories( "exclude=" . MclSettingsHelper::getStatusExcludeCategory() );
+                    foreach ( $categories as $category ) {
+                        ?>
+                        <tr>
+                            <th scope="row"><?php echo $category->name; ?></th>
+                            <td><input type="text" name="<?php echo "mcl_unit_{$category->slug}"; ?>" value="<?php echo esc_attr( get_option( "mcl_unit_{$category->slug}" ) ); ?>" style="width:100%;" />
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </table>
+
+                <?php submit_button(); ?>
+            </form>
+        </div>	
+        <?php
+    }
+
+}
+
+?>
