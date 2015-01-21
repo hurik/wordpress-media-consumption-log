df12e4aa0cf7556f1f35d9a8fb6dfec858f459e9
Andreas Giemza <andreas@giemza.net>
2015-01-21 10:27:48 +0100

    Highly improved queries and size of the saved data (only 43 % of the queries, 62 % of the time and 30 % saved data size)

--- admin/MclRebuildData.php
+++ admin/MclRebuildData.php
@@ -47,7 +47,7 @@ class MclRebuildData {
                     <td><?php timer_stop( 1 ); ?></td>
                 </tr>
             </table>
-        </div>	
+        </div>
         <?php
     }
 
@@ -55,7 +55,7 @@ class MclRebuildData {
         // Set the default timezone
         date_default_timezone_set( get_option( 'timezone_string' ) );
 
-        $data = new StdClass;
+        $data = new stdClass;
 
         // Get the categories
         $categories = get_categories( "include=" . MclSettings::get_monitored_categories_series() . "," . MclSettings::get_monitored_categories_non_series() );
@@ -91,7 +91,14 @@ class MclRebuildData {
         $tags_count_ongoing = 0;
         $tags_count_complete = 0;
 
-        foreach ( $categories as $category ) {
+        $data->categories = array();
+
+        foreach ( $categories as $wp_category ) {
+            $category = new stdClass;
+            $category->term_id = $wp_category->term_id;
+            $category->name = $wp_category->name;
+            $category->slug = $wp_category->slug;
+
             self::get_tags_of_category( $category );
 
             $tags_count_ongoing += $category->mcl_tags_count_ongoing;
@@ -105,9 +112,10 @@ class MclRebuildData {
 
             $consumption_total += $category->mcl_consumption_total;
             $consumption_average += $category->mcl_consumption_average;
+
+            $data->categories[] = $category;
         }
 
-        $data->categories = $categories;
         $data->consumption_total = $consumption_total;
         $data->consumption_average = $consumption_average;
 
@@ -122,18 +130,24 @@ class MclRebuildData {
         global $wpdb;
 
         $tags = $wpdb->get_results( "
-            Select 
+            Select
                 temp.tag_id,
-                temp.cat_id,
-                temp.count,
+                temp.taxonomy,
+                temp.name,
+                temp.post_id,
+                temp.post_date,
+                temp.post_title,
                 IFNULL(mcl.complete, 0) AS complete
-            FROM 
+            FROM
 		(
                     SELECT
                         terms2.term_id AS tag_id,
+                        t2.taxonomy AS taxonomy,
+                        terms2.name AS name,
                         t1.term_id AS cat_id,
-                        COUNT(*) AS count,
-                        terms2.name AS name
+                        p2.ID AS post_id,
+                        p2.post_date,
+                        p2.post_title
                     FROM
                         {$wpdb->prefix}posts AS p1
                         LEFT JOIN {$wpdb->prefix}term_relationships AS r1 ON p1.ID = r1.object_ID
@@ -150,7 +164,11 @@ class MclRebuildData {
                         AND t2.taxonomy = 'post_tag'
                         AND p2.post_status = 'publish'
                         AND p1.ID = p2.ID
-                    GROUP BY name
+                        AND p2.post_date = (
+                            SELECT MAX(p3.post_date)
+                            FROM {$wpdb->prefix}posts AS p3
+                            LEFT JOIN {$wpdb->prefix}term_relationships AS r3 ON p3.ID = r3.object_ID
+                            WHERE r3.term_taxonomy_id = terms2.term_id)
                     ORDER BY name
                 ) AS temp
             LEFT JOIN {$wpdb->prefix}mcl_complete AS mcl ON temp.tag_id = mcl.tag_id AND temp.cat_id = mcl.cat_id
@@ -162,25 +180,20 @@ class MclRebuildData {
         $tags_complete = array();
 
         foreach ( $tags as $tag ) {
-            // Get tag data
-            $tag->tag_data = get_tag( $tag->tag_id );
             // Comma in tags
-            $tag->tag_data = MclCommaInTags::comma_tag_filter( $tag->tag_data );
+            $tag = MclCommaInTags::comma_tag_filter( $tag );
             // Get tag link
-            $tag->tag_link = get_tag_link( $tag->tag_data );
+            $tag->tag_link = get_tag_link( $tag->tag_id );
 
             // Get last post data
-            $post_data = get_posts( "posts_per_page=1&tag_id={$tag->tag_id}&category={$tag->cat_id}" );
-            $tag->post_data = array_shift( $post_data );
-            // Get post link
-            $tag->post_link = get_permalink( $tag->post_data );
+            $tag->post_link = get_permalink( $tag->post_id );
 
             if ( $tag->complete == false ) {
                 $tags_count_ongoing++;
 
                 // Tags which start with a number get their own group #
-                if ( preg_match( '/^[a-z]/i', trim( $tag->tag_data->name[0] ) ) ) {
-                    $tags_ongoing[strtoupper( $tag->tag_data->name[0] )][] = $tag;
+                if ( preg_match( '/^[a-z]/i', trim( $tag->name[0] ) ) ) {
+                    $tags_ongoing[strtoupper( $tag->name[0] )][] = $tag;
                 } else {
                     $tags_ongoing['#'][] = $tag;
                 }
@@ -188,8 +201,8 @@ class MclRebuildData {
                 $tags_count_complete++;
 
                 // Tags which start with a number get their own group #
-                if ( preg_match( '/^[a-z]/i', trim( $tag->tag_data->name[0] ) ) ) {
-                    $tags_complete[strtoupper( $tag->tag_data->name[0] )][] = $tag;
+                if ( preg_match( '/^[a-z]/i', trim( $tag->name[0] ) ) ) {
+                    $tags_complete[strtoupper( $tag->name[0] )][] = $tag;
                 } else {
                     $tags_complete['#'][] = $tag;
                 }
