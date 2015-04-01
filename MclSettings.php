eb173bf98ea10e0a4b44001ac041afe2c8f13bed
Andreas Giemza <andreas@giemza.net>
2015-04-01 18:48:19 +0200

    Settings name now in const

--- MclSettings.php
+++ MclSettings.php
@@ -20,8 +20,20 @@
 
 class MclSettings {
 
-    // Option group name
-    const option_group_name = "mcl-settings-group";
+    // Setting group name
+    const SETTINGS_GROUP = "mcl-settings-group";
+    // Setting names
+    const SETTING_MONITORED_CATEGORIES_SERIAL = "mcl_setting_monitored_categories_serials";
+    const SETTING_MONITORED_CATEGORIES_NON_SERIAL = "mcl_setting_monitored_categories_non_serials";
+    const SETTING_STATISTICS_DAILY_COUNT = "mcl_setting_statistics_daily_count";
+    const SETTING_STATISTICS_DAILY_DATE_FORMAT = "mcl_setting_statistics_daily_date_format";
+    const SETTING_STATISTICS_DAILY_OPTIONS = "mcl_setting_statistics_daily_options";
+    const SETTING_STATISTICS_MONTHLY_COUNT = "mcl_setting_statistics_monthly_count";
+    const SETTING_STATISTICS_MONTHLY_DATE_FORMAT = "mcl_setting_statistics_monthly_date_format";
+    const SETTING_STATISTICS_MONTHLY_OPTIONS = "mcl_setting_statistics_monthly_options";
+    const SETTING_OTHER_SEPARATOR = "mcl_setting_other_separator";
+    const SETTING_OTHER_AND = "mcl_setting_other_and";
+    const SETTING_OTHER_TO = "mcl_setting_other_to";
     // Default values
     const default_statistics_daily_count = 31;
     const default_statistics_daily_options = "annotations: { textStyle: { color: '#000000', fontSize: 9, bold: true }, highContrast: true, alwaysOutside: true },
@@ -59,15 +71,15 @@ isStacked: true,";
 
     // Getter
     public static function get_monitored_categories_serials() {
-        return get_option( 'mcl_setting_monitored_categories_serials' );
+        return get_option( self::SETTING_MONITORED_CATEGORIES_SERIAL );
     }
 
     public static function get_monitored_categories_non_serials() {
-        return get_option( 'mcl_setting_monitored_categories_non_serials' );
+        return get_option( self::SETTING_MONITORED_CATEGORIES_NON_SERIAL );
     }
 
     public static function get_statistics_daily_count() {
-        $value = get_option( 'mcl_setting_statistics_daily_count' );
+        $value = get_option( self::SETTING_STATISTICS_DAILY_COUNT );
 
         if ( ( string ) ( int ) $value === $value && ( int ) $value >= 0 ) {
             return $value;
@@ -77,7 +89,7 @@ isStacked: true,";
     }
 
     public static function get_statistics_daily_date_format() {
-        $value = get_option( 'mcl_setting_statistics_daily_date_format' );
+        $value = get_option( self::SETTING_STATISTICS_DAILY_DATE_FORMAT );
 
         if ( empty( $value ) ) {
             return self::default_statistics_daily_date_format();
@@ -87,7 +99,7 @@ isStacked: true,";
     }
 
     public static function get_statistics_daily_options() {
-        $value = get_option( 'mcl_setting_statistics_daily_options' );
+        $value = get_option( self::SETTING_STATISTICS_DAILY_OPTIONS );
 
         if ( empty( $value ) ) {
             return self::default_statistics_daily_options;
@@ -97,7 +109,7 @@ isStacked: true,";
     }
 
     public static function get_statistics_monthly_count() {
-        $value = get_option( 'mcl_setting_statistics_monthly_count' );
+        $value = get_option( self::SETTING_STATISTICS_MONTHLY_COUNT );
 
         if ( ( string ) ( int ) $value === $value && ( int ) $value >= 0 ) {
             return $value;
@@ -107,7 +119,7 @@ isStacked: true,";
     }
 
     public static function get_statistics_monthly_date_format() {
-        $value = get_option( 'mcl_setting_statistics_monthly_date_format' );
+        $value = get_option( self::SETTING_STATISTICS_MONTHLY_DATE_FORMAT );
 
         if ( empty( $value ) ) {
             return self::default_statistics_monthly_date_format();
@@ -117,7 +129,7 @@ isStacked: true,";
     }
 
     public static function get_statistics_monthly_options() {
-        $value = get_option( 'mcl_setting_statistics_monthly_options' );
+        $value = get_option( self::SETTING_STATISTICS_MONTHLY_OPTIONS );
 
         if ( empty( $value ) ) {
             return self::default_statistics_monthly_options;
@@ -127,7 +139,7 @@ isStacked: true,";
     }
 
     public static function get_other_separator() {
-        $value = get_option( 'mcl_setting_other_separator' );
+        $value = get_option( self::SETTING_OTHER_SEPARATOR );
 
         if ( empty( $value ) ) {
             return " " . trim( self::default_other_separator ) . " ";
@@ -137,7 +149,7 @@ isStacked: true,";
     }
 
     public static function get_other_and() {
-        $value = get_option( 'mcl_setting_other_and' );
+        $value = get_option( self::SETTING_OTHER_AND );
 
         if ( empty( $value ) ) {
             return " " . trim( self::default_other_and() ) . " ";
@@ -147,7 +159,7 @@ isStacked: true,";
     }
 
     public static function get_other_to() {
-        $value = get_option( 'mcl_setting_other_to' );
+        $value = get_option( self::SETTING_OTHER_TO );
 
         if ( empty( $value ) ) {
             return " " . trim( self::default_other_to() ) . " ";
@@ -158,17 +170,17 @@ isStacked: true,";
 
     // Setting page
     public static function register_settings() {
-        register_setting( self::option_group_name, 'mcl_setting_monitored_categories_serials' );
-        register_setting( self::option_group_name, 'mcl_setting_monitored_categories_non_serials' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_daily_count' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_daily_date_format' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_daily_options' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_monthly_count' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_monthly_date_format' );
-        register_setting( self::option_group_name, 'mcl_setting_statistics_monthly_options' );
-        register_setting( self::option_group_name, 'mcl_setting_other_separator' );
-        register_setting( self::option_group_name, 'mcl_setting_other_and' );
-        register_setting( self::option_group_name, 'mcl_setting_other_to' );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_MONITORED_CATEGORIES_SERIAL );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_MONITORED_CATEGORIES_NON_SERIAL );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_DAILY_COUNT );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_DAILY_DATE_FORMAT );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_DAILY_OPTIONS );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_MONTHLY_COUNT );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_MONTHLY_DATE_FORMAT );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_STATISTICS_MONTHLY_OPTIONS );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_OTHER_SEPARATOR );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_OTHER_AND );
+        register_setting( self::SETTINGS_GROUP, self::SETTING_OTHER_TO );
     }
 
     public static function create_page() {
@@ -183,19 +195,19 @@ isStacked: true,";
             <h2>Media Consumption Log - <?php _e( 'Settings', 'media-consumption-log' ); ?></h2>
 
             <form method="post" action="options.php">
-                <?php settings_fields( self::option_group_name ); ?>
-                <?php do_settings_sections( self::option_group_name ); ?>
+                <?php settings_fields( self::SETTINGS_GROUP ); ?>
+                <?php do_settings_sections( self::SETTINGS_GROUP ); ?>
 
                 <h3><?php _e( 'Monitored categories', 'media-consumption-log' ); ?></h3>
                 <table class="form-table">
                     <tr>
                         <th scope="row"><?php _e( 'Serials', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_monitored_categories_serials" value="<?php echo esc_attr( self::get_monitored_categories_serials() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_MONITORED_CATEGORIES_SERIAL; ?>" value="<?php echo esc_attr( self::get_monitored_categories_serials() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'IDs of the categories which have epoisodes or chapters. This categories will be visible in the Statistics, Status, Quick Post and Complete. Example: 2,4,43,50,187,204', 'media-consumption-log' ); ?></p></td>
                     </tr>
                     <tr>
                         <th scope="row"><?php _e( 'Non serials', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_monitored_categories_non_serials" value="<?php echo esc_attr( self::get_monitored_categories_non_serials() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_MONITORED_CATEGORIES_NON_SERIAL; ?>" value="<?php echo esc_attr( self::get_monitored_categories_non_serials() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'IDs of the categories which are visible in Statistics and Status, but not in Quick Post and Complete. Example: 45,75,284', 'media-consumption-log' ); ?></p></td>
                     </tr>
                     <tr>
@@ -208,38 +220,38 @@ isStacked: true,";
                 <table class="form-table">
                     <tr>
                         <th scope="row"><?php _e( 'Daily statistics size', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_statistics_daily_count" value="<?php echo esc_attr( self::get_statistics_daily_count() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_STATISTICS_DAILY_COUNT; ?>" value="<?php echo esc_attr( self::get_statistics_daily_count() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'Please insert number of days the daily statistic should cover. If you insert 0 the days since the first post will be covered. <strong>Attention:</strong> The graph can get really big! Default:', 'media-consumption-log' ); ?> <?php echo self::default_statistics_daily_count; ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'Daily date format', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_statistics_daily_date_format" value="<?php echo esc_attr( self::get_statistics_daily_date_format() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_STATISTICS_DAILY_DATE_FORMAT; ?>" value="<?php echo esc_attr( self::get_statistics_daily_date_format() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'Format for dates on the daily statistics page. Default:', 'media-consumption-log' ); ?> <?php echo self::default_statistics_daily_date_format(); ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'Daily Google Charts Options', 'media-consumption-log' ); ?></th>
-                        <td><textarea name="mcl_setting_statistics_daily_options" rows="6" style="width:100%;"><?php echo esc_attr( self::get_statistics_daily_options() ); ?></textarea>
+                        <td><textarea name="<?php echo self::SETTING_STATISTICS_DAILY_OPTIONS; ?>" rows="6" style="width:100%;"><?php echo esc_attr( self::get_statistics_daily_options() ); ?></textarea>
                             <p class="description"><?php _e( 'When the daily graph gets really big it is sometime necessary to change some Google Charts options. Check the documentation for more information: <a href="https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart#StackedBars">Google Charts - Stacked Bar Charts</a><br />Default:<br />', 'media-consumption-log' ); ?>
                                 <?php echo str_replace( "\n", "<br />", self::default_statistics_daily_options ); ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'Monthly statistics size', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_statistics_monthly_count" value="<?php echo esc_attr( self::get_statistics_monthly_count() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_STATISTICS_MONTHLY_COUNT; ?>" value="<?php echo esc_attr( self::get_statistics_monthly_count() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'Please insert number of months the statistic should cover. If you insert 0 the months since the first post will be covered. Default:', 'media-consumption-log' ); ?> <?php echo self::default_statistics_monthly_count; ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'Monthly date format', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_statistics_monthly_date_format" value="<?php echo esc_attr( self::get_statistics_monthly_date_format() ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_STATISTICS_MONTHLY_DATE_FORMAT; ?>" value="<?php echo esc_attr( self::get_statistics_monthly_date_format() ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'Format for dates on the monthly statistics page. Default:', 'media-consumption-log' ); ?> <?php echo self::default_statistics_monthly_date_format(); ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'Monthly Google Charts Options', 'media-consumption-log' ); ?></th>
-                        <td><textarea name="mcl_setting_statistics_monthly_options" rows="6" style="width:100%;"><?php echo esc_attr( self::get_statistics_monthly_options() ); ?></textarea>
+                        <td><textarea name="<?php echo self::SETTING_STATISTICS_MONTHLY_OPTIONS; ?>" rows="6" style="width:100%;"><?php echo esc_attr( self::get_statistics_monthly_options() ); ?></textarea>
                             <p class="description"><?php _e( 'When the monthly graph gets really big it is sometime necessary to change some Google Charts options. Check the documentation for more information: <a href="https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart#StackedBars">Google Charts - Stacked Bar Charts</a><br />Default:<br />', 'media-consumption-log' ); ?>
                                 <?php echo str_replace( "\n", "<br />", self::default_statistics_monthly_options ); ?></p></td>
                     </tr>
@@ -251,19 +263,19 @@ isStacked: true,";
                 <table class="form-table">
                     <tr>
                         <th scope="row"><?php _e( 'Separator', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_other_separator" value="<?php echo esc_attr( trim( self::get_other_separator() ) ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_OTHER_SEPARATOR; ?>" value="<?php echo esc_attr( trim( self::get_other_separator() ) ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'Define a seperator which separates the title from the episode/chapter number. Spaces are added on both side. Default:', 'media-consumption-log' ); ?> <?php echo self::default_other_separator; ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'mcl_number "and"', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_other_and" value="<?php echo esc_attr( trim( self::get_other_and() ) ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_OTHER_AND; ?>" value="<?php echo esc_attr( trim( self::get_other_and() ) ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'When the keyword is in the episode/chapter number the mcl_number will be set to 2. Spaces are added on both side. Default:', 'media-consumption-log' ); ?> <?php echo self::default_other_and(); ?></p></td>
                     </tr>
 
                     <tr>
                         <th scope="row"><?php _e( 'mcl_number "to"', 'media-consumption-log' ); ?></th>
-                        <td><input type="text" name="mcl_setting_other_to" value="<?php echo esc_attr( trim( self::get_other_to() ) ); ?>" style="width:100%;" />
+                        <td><input type="text" name="<?php echo self::SETTING_OTHER_TO; ?>" value="<?php echo esc_attr( trim( self::get_other_to() ) ); ?>" style="width:100%;" />
                             <p class="description"><?php _e( 'When the keyword is in the episode/chapter number the mcl_number will be set to last number - first number + 1. Spaces are added on both side. Default:', 'media-consumption-log' ); ?> <?php echo self::default_other_to(); ?></p></td>
                     </tr>
                 </table>
