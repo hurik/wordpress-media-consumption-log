008992453b7f738974b0c14854e620236bb402f5
Andreas Giemza <andreas@giemza.net>
2015-04-22 01:35:28 +0200

    Fix for "Trying to get property of non-object" error in MclCommaInTags

--- MclCommaInTags.php
+++ MclCommaInTags.php
@@ -22,17 +22,23 @@ class MclCommaInTags {
 
     public static function comma_tag_filter( $tag_arr ) {
         $tag_arr_new = $tag_arr;
-        if ( $tag_arr->taxonomy == 'post_tag' && strpos( $tag_arr->name, '--' ) ) {
+
+        if ( property_exists( $tag_arr, "taxonomy" ) &&
+                $tag_arr->taxonomy == 'post_tag' &&
+                strpos( $tag_arr->name, '--' ) ) {
             $tag_arr_new->name = str_replace( '--', ', ', $tag_arr->name );
         }
+
         return $tag_arr_new;
     }
 
     public static function comma_tags_filter( $tags_arr ) {
         $tags_arr_new = array();
+
         foreach ( $tags_arr as $tag_arr ) {
             $tags_arr_new[] = self::comma_tag_filter( $tag_arr );
         }
+
         return $tags_arr_new;
     }
 
