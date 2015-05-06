4674b279a6f3c3e127792df1fa9489f363f7acd2
Andreas Giemza <andreas@giemza.net>
2015-05-07 01:06:18 +0200

    Forgotten now also uses MclData

--- MclForgotten.php
+++ MclForgotten.php
@@ -28,26 +28,53 @@ class MclForgotten {
         // Set the default timezone
         date_default_timezone_set( get_option( 'timezone_string' ) );
 
-        $max_date = date( 'Y-m-d H:i:s', strtotime( "-" . (MclSettings::get_forgotten_min_days() - 1) . " day", strtotime( date( 'Y-m-d H:i:s' ) ) ) );
+        // Get the data
+        $data = MclData::get_data();
 
-        $serials = MclSettings::get_monitored_categories_serials();
+        $empty = true;
 
-        if ( empty( $serials ) ) {
-            self::nothing_here_yet();
-            return;
+        foreach ( $data->categories as $category ) {
+            $category->forgotten = array();
+
+            if ( !MclHelper::is_monitored_serial_category( $category->term_id ) || $category->mcl_tags_count_ongoing < 1 ) {
+                continue;
+            }
+
+            foreach ( $category->mcl_tags_ongoing as $letter ) {
+                foreach ( $letter as $tag ) {
+                    $date = DateTime::createFromFormat( "Y-m-d H:i:s", $tag->post_date );
+                    $date_current = new DateTime( date( "Y-m-d H:i:s" ) );
+                    $number_of_days = $date_current->diff( $date )->format( "%a" ) + 1;
+
+                    if ( $number_of_days >= MclSettings::get_forgotten_min_days() ) {
+                        $tag->forgotten = $number_of_days;
+                        $category->forgotten[] = $tag;
+
+                        $empty = false;
+                    }
+                }
+            }
+
+            usort( $category->forgotten, function( $a, $b ) {
+                return $a->forgotten < $b->forgotten;
+            } );
         }
 
-        $categories = get_categories( "include=" . $serials );
+        if ( $empty ) {
+            ?><div class="wrap">
+                <h2>Media Consumption Log - <?php _e( 'Forgotten', 'media-consumption-log' ); ?></h2>
+                <p><strong><?php _e( 'Nothing here yet!', 'media-consumption-log' ); ?></strong></p>
+            </div><?php
+            return;
+        }
 
         $nav = "";
         $nav_first = true;
 
         $html = "";
 
-        foreach ( $categories as $category ) {
-            $tags = self::get_serials( $category, $max_date );
-
-            if ( count( $tags ) < 1 ) {
+        foreach ( $data->categories as $category ) {
+            if ( count( $category->forgotten ) < 1 ) {
                 continue;
             }
 
@@ -59,7 +86,7 @@ class MclForgotten {
 
             $nav .= "<a href=\"#mediastatus-{$category->slug}\">{$category->name}</a>";
 
-            $html .= "\n\n<div class=\"anchor\" id=\"mediastatus-{$category->slug}\"></div><h3>{$category->name} (" . count( $tags ) . ")</h3><hr />"
+            $html .= "\n\n<div class=\"anchor\" id=\"mediastatus-{$category->slug}\"></div><h3>{$category->name} (" . count( $category->forgotten ) . ")</h3><hr />"
                     . "\n<table class=\"widefat\">"
                     . "\n  <colgroup>"
                     . "\n    <col width=\"49%\">"
@@ -72,31 +99,21 @@ class MclForgotten {
                     . "\n    <th nowrap><strong>" . __( 'Days ago', 'media-consumption-log' ) . "</strong></th>"
                     . "\n  </tr>";
 
-            MclCommaInTags::comma_tags_filter( $tags );
+            MclCommaInTags::comma_tags_filter( $category->forgotten );
 
-            foreach ( $tags as $tag ) {
-                $tag_link = get_tag_link( $tag->tag_id );
+            foreach ( $category->forgotten as $tag ) {
                 $tag_title = htmlspecialchars( $tag->name );
                 $status = MclHelper::get_last_consumed( $tag->post_title );
                 $post_title = htmlspecialchars( $tag->post_title );
-                $status_link = get_permalink( $tag->post_id );
-                $date = DateTime::createFromFormat( "Y-m-d H:i:s", $tag->post_date );
-                $date_current = new DateTime( date( "Y-m-d H:i:s" ) );
-                $number_of_days = $date_current->diff( $date )->format( "%a" ) + 1;
 
                 $html .= "\n  <tr>"
-                        . "\n    <td><a href=\"{$tag_link}\" title=\"{$tag_title}\">{$tag_title}</a></td>"
-                        . "\n    <td><a href=\"{$status_link}\" title=\"{$post_title}\">{$status}</a></td>"
-                        . "\n    <td nowrap>{$number_of_days}</td>"
+                        . "\n    <td><a href=\"{$tag->tag_link}\" title=\"{$tag_title}\">{$tag_title}</a></td>"
+                        . "\n    <td><a href=\"{$tag->post_link}\" title=\"{$post_title}\">{$status}</a></td>"
+                        . "\n    <td nowrap>{$tag->forgotten}</td>"
                         . "\n  </tr>";
             }
 
             $html .= "\n</table>";
-        }
-
-        if ( empty( $nav ) ) {
-            self::nothing_here_yet();
-            return;
         }
         ?><div class="wrap">
             <h2>Media Consumption Log - <?php _e( 'Forgotten', 'media-consumption-log' ); ?></h2>
@@ -113,78 +130,6 @@ class MclForgotten {
 
             <div class="mcl_css_back_to_top">^</div>
         </div><?php
-    }
-
-    private static function nothing_here_yet() {
-        ?><div class="wrap">
-            <h2>Media Consumption Log - <?php _e( 'Forgotten', 'media-consumption-log' ); ?></h2>
-            <p><strong><?php _e( 'Nothing here yet!', 'media-consumption-log' ); ?></strong></p>
-        </div><?php
-    }
-
-    private static function get_serials( $category, $max_date ) {
-        global $wpdb;
-
-        $tags = $wpdb->get_results( "
-            Select
-                temp.tag_id,
-                temp.taxonomy,
-                temp.name,
-                temp.post_id,
-                temp.post_date,
-                temp.post_title
-            FROM
-		(
-                    SELECT
-                        terms2.term_id AS tag_id,
-                        t2.taxonomy AS taxonomy,
-                        terms2.name AS name,
-                        t1.term_id AS cat_id,
-                        p2.ID AS post_id,
-                        p2.post_date,
-                        p2.post_title
-                    FROM
-                        {$wpdb->prefix}posts AS p1
-                        LEFT JOIN {$wpdb->prefix}term_relationships AS r1 ON p1.ID = r1.object_ID
-                        LEFT JOIN {$wpdb->prefix}term_taxonomy AS t1 ON r1.term_taxonomy_id = t1.term_taxonomy_id
-                        LEFT JOIN {$wpdb->prefix}terms AS terms1 ON t1.term_id = terms1.term_id,
-                        {$wpdb->prefix}posts AS p2
-                        LEFT JOIN {$wpdb->prefix}term_relationships AS r2 ON p2.ID = r2.object_ID
-                        LEFT JOIN {$wpdb->prefix}term_taxonomy AS t2 ON r2.term_taxonomy_id = t2.term_taxonomy_id
-                        LEFT JOIN {$wpdb->prefix}terms AS terms2 ON t2.term_id = terms2.term_id
-                    WHERE
-                        t1.taxonomy = 'category'
-                        AND p1.post_status = 'publish'
-                        AND terms1.term_id = '{$category->term_id}'
-                        AND t2.taxonomy = 'post_tag'
-                        AND p2.post_status = 'publish'
-                        AND p1.ID = p2.ID
-                        AND p2.post_date < '{$max_date}'
-                        AND p2.post_date = (
-                            SELECT
-                                MAX(dp1.post_date)
-                            FROM
-                                {$wpdb->prefix}posts AS dp1
-                                LEFT JOIN {$wpdb->prefix}term_relationships AS dr1 ON dp1.ID = dr1.object_ID
-                                LEFT JOIN {$wpdb->prefix}term_taxonomy AS dt1 ON dr1.term_taxonomy_id = dt1.term_taxonomy_id,
-                                {$wpdb->prefix}posts AS dp2
-                                LEFT JOIN {$wpdb->prefix}term_relationships AS dr2 ON dp2.ID = dr2.object_ID
-                                LEFT JOIN {$wpdb->prefix}term_taxonomy AS dt2 ON dr2.term_taxonomy_id = dt2.term_taxonomy_id
-                            WHERE
-                                dp1.ID = dp2.ID
-                                AND dt1.taxonomy = 'category'
-                                AND dp1.post_status = 'publish'
-                                AND dt2.taxonomy = 'post_tag'
-                                AND dp2.post_status = 'publish'
-                                AND dt1.term_id = t1.term_id
-                                AND dt2.term_id = t2.term_id)
-                    ORDER BY post_date
-                ) AS temp
-            LEFT JOIN {$wpdb->prefix}mcl_status AS mcl ON temp.tag_id = mcl.tag_id AND temp.cat_id = mcl.cat_id
-            WHERE IFNULL(status, 0) = 0
-	" );
-
-        return $tags;
     }
 
 }
